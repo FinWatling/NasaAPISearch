9db33682c084e3bbdfd9647a4cc8c5fe967087ff
finlaywatling <finlaywatling@gmail.com>
2022-02-28 13:09:32 +0000

    added input and button

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 window.addEventListener("load", () => {
 
+searchButton = document.querySelector('#search-button');
 
+searchButton.addEventListener("click", () =>{
+
+alert("Button Clicked")
+
+})
 
 
 
