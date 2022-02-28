2ba1582de8f7d81781b891a4900a40008320de45
finlaywatling <finlaywatling@gmail.com>
2022-02-28 16:26:42 +0000

    created fetch request, currently returning json object. working on specifying for image

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 window.addEventListener("load", () => {
 
 searchButton = document.querySelector('#search-button');
+searchBar = document.querySelector('#search-bar');
+resultImage = document.querySelector('.result-image')
 
-searchButton.addEventListener("click", () =>{
+var url = 'https://images-api.nasa.gov/search?q='
 
-alert("Button Clicked")
+const requestOptions = {
+
+    method: 'GET',
+    headers: {'Content-Type' : 'application/json'},
+
+}
+
+searchButton.addEventListener("click", () => {
+
+    var query = searchBar.value;
+
+    var encodedURI = url + encodeURIComponent(query) + '&media_type=image';
+    
+    console.log('searching at: ' + encodedURI);
+
+    fetch(encodedURI,requestOptions)
+    .then(res => {
+
+        res = res.json();
+
+        console.log(res.status)
+        console.log(res)
+
+        changeImage(res);
+
+
+    })
+
+    function changeImage(response){
+
+        var path = response.collection.items[0].links[0].href
+
+        resultImage.src = path;
+
+    }
+
+
+
+
+
 
 })
 
