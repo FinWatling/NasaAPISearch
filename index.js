e9352bf90539bf7b18abbd5aa0defbf67aff39d9
finlaywatling <finlaywatling@gmail.com>
2022-03-04 17:17:20 +0000

    a lot of css changes

--- index.js
+++ index.js
@@ -1,44 +1,62 @@
 window.addEventListener("load", () => {
 
-searchButton = document.querySelector('#search-button');
-searchBar = document.querySelector('#search-bar');
-searchResultsWrapper = document.querySelector('#search-results-wrapper')
-numberOfResults = document.querySelector('#number-of-results')
+    searchButton = document.querySelector('#search-button');
+    searchBar = document.querySelector('#search-bar');
+    searchResultsWrapper = document.querySelector('#search-results-wrapper')
+    numberOfResults = document.querySelector('#number-of-results')
 
-var url = 'https://images-api.nasa.gov/search?q='
+    var url = 'https://images-api.nasa.gov/search?q='
 
-const requestOptions = {
+    const requestOptions = {
 
-    method: 'GET',
-    headers: {'Content-Type' : 'application/json'},
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
 
-}
+    }
 
-searchButton.addEventListener("click", () => {
+    searchButton.addEventListener("click", () => {
 
-    var query = searchBar.value;
+        var query = searchBar.value;
 
-    var encodedURI = url + encodeURIComponent(query) + '&media_type=image';
-    
-    console.log('searching at: ' + encodedURI);
+        var encodedURI = url + encodeURIComponent(query) + '&media_type=image';
 
-    fetch(encodedURI,requestOptions)
-    .then(res => res.json())
-    .then(data => {
+        console.log('searching at: ' + encodedURI);
 
-
-        console.log(data.status);
-        console.log(data);
-
-        search(data);
+        fetch(encodedURI, requestOptions)
+            .then(res => res.json())
+            .then(data => {
 
 
-    })
+                console.log(data.status);
+                console.log(data);
 
-    function search(data){
+                search(data,query);
 
-        var path;
 
+            })
+
+        })
+
+
+
+        function search(data,query) {
+
+            var path;
+
+            if (query === "" || query === " ") { //get this working (in case of no input)
+
+                numberOfResults.textContent = "Please enter a search query "
+
+                return null;
+            }
+
+            if (data.collection.items.length >= 100) {
+                numberOfResults.textContent = "Your search for \"" + query + "\" returned more than " + data.collection.items.length + " results "
+            } else if (data.collection.items.length > 0) {
+                numberOfResults.textContent = "Your search for \"" + query + "\" returned " + data.collection.items.length + " results "
+            } else {
+                numberOfResults.textContent = "No results found. "
+            }
 
 
             while (searchResultsWrapper.lastChild) {
@@ -46,83 +64,60 @@ searchButton.addEventListener("click", () => {
             }
 
 
-        const resultsSet = new Set();
+            const resultsSet = new Set();
 
-        for(i=0;i<data.collection.items.length;i++){
+            for (i = 0; i < data.collection.items.length; i++) {
 
-            if (resultsSet.size == 0){
+                if (resultsSet.has(data.collection.items[i].links[0].href)) {
+
+                    continue; //to prevent duplicate images
+
+                }
+
                 resultsSet.add(data.collection.items[i].links[0].href);
-            }else if(resultsSet.has(data.collection.items[i].links[0].href)){
-                continue;
+
+
+                var resultCard = document.createElement("div");
+                var infoSection = document.createElement("div");
+                var imgcontainer = document.createElement("div");
+                var img = document.createElement("img");
+                var title = document.createElement("h3");
+                var date = document.createElement("h6");
+
+                img.className = "result-image"
+                resultCard.className = "result-card";
+                infoSection.className = "info-section";
+                imgcontainer.className = "img-container"; 9
+
+
+                path = data.collection.items[i].links[0].href;
+
+                title.textContent = data.collection.items[i].data[0].title;
+
+                img.src = path;
+
+                searchResultsWrapper.appendChild(resultCard);
+                resultCard.appendChild(img);
+                resultCard.appendChild(infoSection);
+                infoSection.appendChild(title);
+
+
+
+
             }
 
-            console.log(resultsSet)
-
-            
-
-
-            var resultCard = document.createElement("div");
-            var infoSection = document.createElement("div");
-            var imgcontainer = document.createElement("div");
-            var img = document.createElement("img");
-            var title = document.createElement("h3");
-            var date = document.createElement("h6");
-
-            
-            resultCard.className = "result-card";
-            infoSection.className = "info-section";
-            imgcontainer.className = "img-container";9
-
-            
-            
 
 
 
-            path = data.collection.items[i].links[0].href;
-
-            title.textContent = data.collection.items[i].data[0].title;
-
-            
-
-            img.src = path;
-
-            searchResultsWrapper.appendChild(resultCard);
-            resultCard.appendChild(img);
-            resultCard.appendChild(infoSection);
-            infoSection.appendChild(title);
-
-    
 
 
-        }
 
-        if(data.collection.items.length >= 100){
-        numberOfResults.textContent = "Your search for \"" +  query  + "\" returned more than " + data.collection.items.length + " results:"
-        } else if(data.collection.items.length > 0){
-            numberOfResults.textContent = "Your search for \"" +  query  + "\" returned " + data.collection.items.length + " results:"
-        }else{
-            numberOfResults.textContent = "No results found."
         }
 
 
 
 
 
-    }
 
+    })
 
-
-
-
-
-})
-
-
-
-
-
-
-
-
-
-})
