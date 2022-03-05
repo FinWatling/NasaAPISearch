24c9bf809b386ca078bf72227c695a98d67969be
finlaywatling <finlaywatling@gmail.com>
2022-03-05 15:43:47 +0000

    accessibility fixes

--- index.js
+++ index.js
@@ -30,84 +30,81 @@ window.addEventListener("load", () => {
                 console.log(data.status);
                 console.log(data);
 
-                search(data,query);
+                search(data, query);
 
 
             })
 
-        })
+    })
 
 
 
-        function search(data,query) {
+    function search(data, query) {
 
-            var path;
+        var path;
 
-            if (query === "" || query === " ") { //get this working (in case of no input)
+        if (query === "" || query === " ") { //get this working (in case of no input)
 
-                numberOfResults.textContent = "Please enter a search query "
+            numberOfResults.textContent = "Please enter a search query "
 
-                return null;
-            }
+            return null;
+        }
 
-            if (data.collection.items.length >= 100) {
-                numberOfResults.textContent = "Your search for \"" + query + "\" returned more than " + data.collection.items.length + " results "
-            } else if (data.collection.items.length > 0) {
-                numberOfResults.textContent = "Your search for \"" + query + "\" returned " + data.collection.items.length + " results "
-            } else {
-                numberOfResults.textContent = "No results found. "
-            }
-
-
-            while (searchResultsWrapper.lastChild) {
-                searchResultsWrapper.removeChild(searchResultsWrapper.lastChild);
-            }
+        if (data.collection.items.length >= 100) {
+            numberOfResults.style.display = "block";
+            numberOfResults.textContent = "Your search for \"" + query + "\" returned more than " + data.collection.items.length + " results "
+        } else if (data.collection.items.length > 0) {
+            numberOfResults.style.display = "block";
+            numberOfResults.textContent = "Your search for \"" + query + "\" returned " + data.collection.items.length + " results "
+        } else {
+            numberOfResults.style.display = "block";
+            numberOfResults.textContent = "No results found. "
+        }
 
 
-            const resultsSet = new Set();
-
-            for (i = 0; i < data.collection.items.length; i++) {
-
-                if (resultsSet.has(data.collection.items[i].links[0].href)) {
-
-                    continue; //to prevent duplicate images
-
-                }
-
-                resultsSet.add(data.collection.items[i].links[0].href);
+        while (searchResultsWrapper.lastChild) {
+            searchResultsWrapper.removeChild(searchResultsWrapper.lastChild);
+        }
 
 
-                var resultCard = document.createElement("div");
-                var infoSection = document.createElement("div");
-                var imgcontainer = document.createElement("div");
-                var img = document.createElement("img");
-                var title = document.createElement("h3");
-                var date = document.createElement("h6");
+        const resultsSet = new Set();
 
-                img.className = "result-image"
-                resultCard.className = "result-card";
-                infoSection.className = "info-section";
-                imgcontainer.className = "img-container"; 9
+        for (i = 0; i < data.collection.items.length; i++) {
 
+            if (resultsSet.has(data.collection.items[i].links[0].href)) {
 
-                path = data.collection.items[i].links[0].href;
-
-                title.textContent = data.collection.items[i].data[0].title;
-
-                img.src = path;
-
-                searchResultsWrapper.appendChild(resultCard);
-                resultCard.appendChild(img);
-                resultCard.appendChild(infoSection);
-                infoSection.appendChild(title);
-
-
-
+                continue; //to prevent duplicate images
 
             }
 
+            resultsSet.add(data.collection.items[i].links[0].href);
 
 
+            var resultCard = document.createElement("div"),
+                infoSection = document.createElement("div"),
+                imgcontainer = document.createElement("div"),
+                img = document.createElement("img"),
+                title = document.createElement("h3"),
+                date = document.createElement("p");
+
+            img.className = "result-image";
+            resultCard.className = "result-card";
+            infoSection.className = "info-section";
+            imgcontainer.className = "img-container";
+
+
+            path = data.collection.items[i].links[0].href;
+
+            title.textContent = data.collection.items[i].data[0].title;
+            date.textContent = "Timestamp: " + data.collection.items[i].data[0].date_created;
+
+            img.src = path;
+
+            searchResultsWrapper.appendChild(resultCard);
+            resultCard.appendChild(img);
+            resultCard.appendChild(infoSection);
+            infoSection.appendChild(title);
+            infoSection.appendChild(date);
 
 
 
@@ -119,5 +116,13 @@ window.addEventListener("load", () => {
 
 
 
-    })
+
+    }
+
+
+
+
+
+
+})
 
